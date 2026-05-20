4039f3e77d72767ead400678a2c38b65813e2f5d
Quickaxis <quickaxis@example.com>
2026-05-20 11:02:26 +0530

    Integrate spotlight-card component and add spotlight border glow animation to room cards in index.html

--- components/ui/demo.tsx
+++ components/ui/demo.tsx
@@ -1,5 +1,22 @@
 import { SparklesText } from "@/components/ui/sparkles-text"
+import { GlowCard } from "@/components/ui/spotlight-card";
 
 export function SparklesTextDemo() {
   return <SparklesText text="Magic UI" />;
+}
+
+export function Default() {
+  return (
+    <div className="w-screen h-screen flex flex-row items-center justify-center gap-10 custom-cursor">
+      <GlowCard>
+        <div className="text-white text-center">White Lotus Card Content</div>
+      </GlowCard>
+      <GlowCard>
+        <div className="text-white text-center">Blue Lilly Card Content</div>
+      </GlowCard>
+      <GlowCard>
+        <div className="text-white text-center">Rose Gold Card Content</div>
+      </GlowCard>
+    </div>
+  );
 }
